9f2ea6a13dc4ae83433e7fdd79e7acbc0e84cf1d
Andrew Vanderpool <avanderpool@phoenixcon.com>
2018-01-17 17:09:43 +0100

    Project 3
    Day 11

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,16 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-};
-
-let board = [[' ',' ',' '],
-             [' ',' ',' '],
-             [' ',' ',' ']];
-
-
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+//I want to refactor this to not be two nested for loops. It would be better as
+//one for loop and then the other, to copy what was created in the first.
+//for example a 5x5 would take 25 steps with nested, and 10 with sequential
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  var board = [];
+  for (var i = 0; i < numberOfRows; i++) {
+    var row = [];
+    for (var j = 0; j < numberOfColumns;j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+}
+var blankBoard = generatePlayerBoard(30,3);
+console.log(blankBoard);
