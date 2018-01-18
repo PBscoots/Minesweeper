efdd0b380acdbf0dcb04022232f2ad6969d69988
Andrew Vanderpool <avanderpool@phoenixcon.com>
2018-01-18 11:20:09 +0100

    Project 3
    Day 11 finished

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -12,5 +12,40 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   }
   return board;
 }
-var blankBoard = generatePlayerBoard(30,3);
-console.log(blankBoard);
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  var board = [];
+  for (var i = 0; i < numberOfRows; i++) {
+    var row = [];
+    for (var j = 0; j < numberOfColumns;j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  var numberOfBombsPlaced = 0;
+  var count = 0;//to look and see how many times it has to try again to place a bomb
+  while (numberOfBombsPlaced < numberOfBombs) {
+    //will use control flow to fix the possiblity to choose the same spot twice
+    var randomRowIndex  = Math.floor(Math.random()*numberOfRows);
+    var randomColumnIndex  = Math.floor(Math.random()*numberOfColumns);
+    if (board[randomRowIndex][randomColumnIndex]  != 'B') {
+      numberOfBombsPlaced++;
+    }
+    board[randomRowIndex][randomColumnIndex]  = 'B';
+    count++;
+  }
+  console.log(count);
+  return board;
+
+}
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard   = generateBombBoard(30,30,50);
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
