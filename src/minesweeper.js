17d4fc421ea247d283b4b3ee3acc51618bf38b73
PBscoots <vanderpool.andrew@gmail.com>
2018-01-22 22:56:11 +0100

    fixed refactor
    fixed my mistakes with misunderstanding the way functions assign to variables

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,7 @@
 //I want to refactor this to not be two nested for loops. It would be better as
 //one for loop and then the other, to copy what was created in the first.
 //for example a 5x5 would take 25 steps with nested, and 10 with sequential
-const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+const generateBoard = (numberOfRows, numberOfColumns) => {
   var board = [];
   for (var i = 0; i < numberOfRows; i++) {
     var row = [];
@@ -11,14 +11,13 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     board.push(row);
   }
   return board;
+};
 
-}
-
-const generateBombBoard = (board, numberOfBombs) => {
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = generateBoard(numberOfRows, numberOfColumns);
   let numberOfBombsPlaced = 0;
   var count = 0;//to look and see how many times it has to try again to place a bomb
   while (numberOfBombsPlaced < numberOfBombs) {
-    //will use control flow to fix the possiblity to choose the same spot twice
     var randomRowIndex  = Math.floor(Math.random()*board.length);
     var randomColumnIndex  = Math.floor(Math.random()*board[0].length);
     if (board[randomRowIndex][randomColumnIndex]  != 'B') {
@@ -29,7 +28,7 @@ const generateBombBoard = (board, numberOfBombs) => {
   }
   console.log('Times tried ' + count);
   return board;
-}
+};
 
 const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   const neighborOffsets = [[-1,1],[0,1],[1,1],[-1,0],[1,0],[-1,-1],[0,-1],[1,-1]];
@@ -46,7 +45,7 @@ const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
     }
   });
   return numberOfBombs;
-}
+};
 
 const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
   if (playerBoard[rowIndex][columnIndex] !== ' ') {
@@ -58,14 +57,14 @@ const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
   } else {
     playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
   }
-}
+};
 
 const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
-}
+};
 
-let playerBoard = generatePlayerBoard(3,3);
-let bombBoard   = generateBombBoard(playerBoard,3);
+let playerBoard = generateBoard(3,3);
+let bombBoard   = generateBombBoard(3,3,3);
 
 console.log('Player Board:');
 printBoard(playerBoard);
